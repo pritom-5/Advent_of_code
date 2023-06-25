5dfee8f05fcbec66b02aaf5e87a405480cfdadf5
pritom <pritom@pop-os.localdomain>
2023-06-26 00:10:04 +0600

    2015-12 part-1 done

---
+++ src/2015/12/12_get_value.ts
@@ -0,0 +1,61 @@
+import { readFileSync } from "fs";
+
+const input = readFileSync("./12_input.txt", "utf8");
+
+function numberSum(input: any) {
+	const regex = /-?\d+/g;
+	const valuesArr = input.match(regex);
+	let result = 0;
+	for (let i of valuesArr) {
+		result += Number(i);
+	}
+
+	return result;
+}
+
+let input_0 = input;
+function removeObjWithRed() {
+	let slice_start = 0;
+	while (true) {
+		let reset = false;
+		const redIndex = input_0.slice(slice_start).indexOf("red");
+
+		console.log(redIndex);
+
+		if (redIndex === -1) {
+			break;
+		}
+
+		let leftIndex = redIndex;
+		let rightIndex = redIndex;
+
+		while (input_0[rightIndex] !== "}") {
+			if (input_0[rightIndex] === "]") {
+				reset = true;
+				break;
+			}
+			++rightIndex;
+		}
+
+		if (reset) {
+			continue;
+		}
+
+		while (input_0[leftIndex] !== "{") {
+			--leftIndex;
+		}
+
+		console.log("--------------------------------------");
+		console.log(input_0.slice(leftIndex, rightIndex + 1));
+		console.log("--------------------------------------");
+
+		input_0 = input_0.replace(
+			input_0.slice(leftIndex, rightIndex + 1),
+			"R"
+		);
+	}
+}
+
+removeObjWithRed();
+
+console.log(numberSum(input_0));
